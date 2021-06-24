ae13c8ed109d7c8aa839003dce4405d90ac41f69
Elizabeth Ke <elizabethke@fb.com>
2021-06-24 12:13:20 -0400

    made basic photo feed

---
+++ README.md
@@ -0,0 +1,1 @@
+basic Tumblr feed iOS app
---
+++ Tumblr/Views/PhotoCell.h
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by Elizabeth Ke on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
